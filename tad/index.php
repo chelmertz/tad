e422b906f7e94a502851ec3011cdcc1c0c3f4737
Carl Helmertz <helmertz@gmail.com>
2013-04-20 03:02:14 +0200

    api/mvc: Open up as API, split functions by responsibility
    Borrowing python's "if __name__ == '__main__'" style, to only execute
    controller logic if we're being called directly.
    
    Introducing an API with namespace + clear modelish function, so that you
    could do:
    
    require 'path/to/tad/index.php';
    $paste_id = \tad\create("this is a paste");
    echo \tad\read($paste_id);
    
    Insight: namespaces should be able to have private functions
    Insight 2: my first php project in X years without OOP. Cheers.
    
    Signed-off-by: Carl Helmertz <helmertz@gmail.com>

--- tad/index.php
+++ tad/index.php
@@ -1,11 +1,13 @@
 <?php
 
+namespace tad;
+
 // LICENSE: Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
 
-// config
+// config, edit as you please
 
-define('PASTE_FOLDER', __DIR__);
-define('LIST_AMOUNT', 20);
+define(__NAMESPACE__.'\PASTE_FOLDER', __DIR__);
+define(__NAMESPACE__.'\LIST_AMOUNT', 20);
 error_reporting(E_ALL|E_STRICT);
 
 // helpers
@@ -38,8 +40,6 @@ function time_ago($timestamp) {
 	return $time_ago.' ago';
 }
 
-// model logic
-
 function check_prereq() {
 	$path = realpath(PASTE_FOLDER);
 	if(!is_writable($path)) {
@@ -54,41 +54,36 @@ function check_prereq() {
 	return null;
 }
 
-function handle_post() {
-	if(isset($_SERVER) && $_SERVER['REQUEST_METHOD'] != "POST") {
-		return;
-	}
-	$body = file_get_contents('php://input');
-	if(isset($_POST['body'])) {
-		$body = $_POST['body'];
-	}
-	if($body) {
-		return redirect(create_paste($body));
-	}
-	return redirect();
+function redirect($id) {
+	$permalink = permalink($id);
+	header("Location: ".$permalink);
+	echo $permalink;
+	exit(0);
 }
 
-function get_search_results() {
-	if(isset($_GET['search']) && $s = $_GET['search']) {
-		exec("grep -ci ".escapeshellarg($s)." ".PASTE_FOLDER."/* | grep -v index.php | egrep -v ':0$' | sort -nr -k2 -t:", $hits, $exit_code);
-		// search all files but index.php in PASTE_FOLDER
-		// exclude files not matching
-		// sort by most hits descending
-		$result = array();
-		foreach($hits as $hit) {
-			preg_match('~/(?P<hash>[a-z0-9]+):(?P<count>\d+)$~', $hit, $parts);
-			$result[] = "<li><a href='".permalink($parts['hash'])."'>".$parts['hash']."</a> (".$parts['count']." hits)</li>";
-		}
-		if(!$result) {
-			return "<h3>No results found for <em>".htmlentities($s, ENT_QUOTES, 'UTF-8')."</em>";
-		}
-		$amount = count($result);
-		return  "<h3>".$amount." result".($amount > 1 ? 's' : null)." found for <em>".htmlentities($s, ENT_QUOTES, 'UTF-8')."</em></h3><ol>".implode("\n", $result)."</ol>";
+// model logic
+
+function search($query = null) {
+	exec("grep -ci ".escapeshellarg($query)." ".PASTE_FOLDER."/* | grep -v index.php | egrep -v ':0$' | sort -nr -k2 -t:", $hits, $exit_code);
+	if(!$hits) {
+		return array();
 	}
-	return null;
+	// search all files but index.php in PASTE_FOLDER
+	// exclude files not matching
+	// sort by most (lines including -) hits, descending
+	$result = array();
+	foreach($hits as $hit) {
+		preg_match('~/(?P<hash>[a-z0-9]+):(?P<count>\d+)$~', $hit, $parts);
+		if(!$parts) {
+			// not a paste
+			continue;
+		}
+		$result[$parts['hash']] = $parts['count'];
+	}
+	return $result;
 }
 
-function create_paste($body) {
+function create($body) {
 	$id = substr(md5($body), 0, 7);
 	$filename = PASTE_FOLDER.'/'.$id;
 	if(!file_exists($filename) && !file_put_contents($filename, $body)) {
@@ -97,26 +92,39 @@ function create_paste($body) {
 	return $id;
 }
 
-function redirect($id) {
-	$permalink = permalink($id);
-	header("Location: ".$permalink);
-	echo $permalink;
-	exit(0);
-}
-
-function list_pastes($count = LIST_AMOUNT) {
+function get($count = LIST_AMOUNT) {
 	exec("ls -t ".PASTE_FOLDER." | grep -v index.php | head -n ".((int)$count), $files, $exit_code);
 	$output = array();
 	foreach($files as $file) {
+		if(!preg_match('~^[a-z0-9]+$~', $file)) {
+			// not a paste
+			continue;
+		}
 		$output[$file] = filemtime(PASTE_FOLDER."/$file");
 	}
 	return $output;
 }
 
+function read($id) {
+	return (string) file_get_contents(PASTE_FOLDER.'/'.$id);
+}
+
 // view logic
 
+function render_search_results($query, $results = array()) {
+	$html = null;
+	foreach($results as $hash => $count) {
+		$html .= "<li><a href='".permalink($hash)."'>$hash</a> ($count hits)</li>\n";
+	}
+	if(!$html) {
+		return "<h3>No results found for <em>".htmlentities($query, ENT_QUOTES, 'UTF-8')."</em>";
+	}
+	$amount = count($results);
+	return  "<h3>".$amount." result".($amount > 1 ? 's' : null)." found for <em>".htmlentities($query, ENT_QUOTES, 'UTF-8')."</em></h3><ol>$html</ol>";
+}
+
 function render_index($search_result = array()) {
-	$pastes = list_pastes();
+	$pastes = get();
 	if(!$pastes) {
 		$last = "<h2>Nothing dumped yet</h2>";
 	} else {
@@ -225,11 +233,36 @@ POLICE;
 }
 
 // controller logic
-if($req = check_prereq()) {
-	header("HTTP/1.0 500 Wrong permissions");
-	echo $req;
-	exit(1);
+
+function handle_post() {
+	if(isset($_SERVER) && $_SERVER['REQUEST_METHOD'] != "POST") {
+		return;
+	}
+	$body = file_get_contents('php://input');
+	if(isset($_POST['body'])) {
+		$body = $_POST['body'];
+	}
+	if($body) {
+		return redirect(create($body));
+	}
+	return redirect();
 }
 
-handle_post();
-render_index(get_search_results());
+// only execute if we're on page
+// using it as an API would be to require index.php and then
+// $pastes = \tad\search('my_string');
+if(isset($_SERVER) && realpath($_SERVER['SCRIPT_FILENAME']) == __FILE__) {
+	// realpath() takes care of symlinked paths
+	if($req = check_prereq()) {
+		header("HTTP/1.0 500 Wrong permissions");
+		echo $req;
+		exit(1);
+	}
+
+	handle_post();
+	$search = null;
+	if(isset($_GET['search']) && $query = $_GET['search']) {
+		$search = render_search_results($query, search($query));
+	}
+	render_index($search);
+}
